5544b0700a7e5ded30651281ded5cb0c41628388
telinak <78906896+telinak@users.noreply.github.com>
2021-03-28 18:42:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,15 +1,15 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int k=0;
-  cin>>"Size of array:">>size;
-  for(int i = 0; i < size; ++i)
-    cin>>arr[i];
-  cin>>"Wanted number">> value;
+  int k = 0;
+  cin >> "Size of array:" >> size;
+  for (int i = 0; i < size; ++i)
+    cin >> arr[i];
+  cin >> "Wanted number" >> value;
   for (int i = 0; i < size; ++i) {
     if (arr[i] == value)
-      k= k + 1;
+      k = k + 1;
   }
-  cout<<"Count of wanted numbers in array:"<< k;
+  cout << "Count of wanted numbers in array:" << k;
   return 0; // если ничего не найдено
 }
