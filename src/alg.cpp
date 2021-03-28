24b2f5dbb6482b5afb10a1831cd44c13f4dbac87
telinak <78906896+telinak@users.noreply.github.com>
2021-03-28 19:48:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,10 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
 int cbinsearch(int *arr, int size, int value) {
-  int count = 0;
+  int k = 0;
   for (int i = 0; i < size; ++i) {
     if (arr[i] == value)
-      count = count + 1;
+      k = k + 1;
   }
-  std::cout << "Count of wanted numbers in array:" << count;
+  return k;
 }
