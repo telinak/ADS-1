44a1ef48a68da3c1a8fd289acbe813e0a7b618bf
telinak <78906896+telinak@users.noreply.github.com>
2021-03-31 16:44:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,4 @@
 // Copyright 2021 NNTU-CS
-#include <iostream>
 int cbinsearch(int *arr, int size, int value) {
   int left = 0, first = -1, count, last, right = size - 1, k = 0, middle;
   while (first < 0) {
@@ -13,14 +12,5 @@ int cbinsearch(int *arr, int size, int value) {
     if (arr[middle] < value)
       left = middle + 1;
   }
-  for (int i = 0; first > 0; ++i) {
-    if (arr[first] == value)
-      first -= 1;
-  }
-  for (int j = 0; last < size; ++j) {
-    if (arr[last] == value)
-      last += 1;
-  }
-  count = last - first + 1;
-  return count;
+  return first;
 }
