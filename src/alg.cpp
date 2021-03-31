b7d6c75e78b8428b6c5d82cc55aaf18a3c9aa977
telinak <78906896+telinak@users.noreply.github.com>
2021-03-31 11:23:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,21 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
 int cbinsearch(int *arr, int size, int value) {
-  int k = 0;
-  for (int i = 0; i < size; ++i) {
-    if (arr[i] == value)
-      k = k + 1;
+  int l = 0, r = size - 1;
+  while (r > l) {
+    int m = (l + r) / 2;
+    if (arr[m] < value) {
+      l = m + 1;
+    } else 
+      if (arr[m] > value) {
+        r = m - 1;
+      } else {
+        return m;
+      }
   }
-  return k;
+  if (arr[l] == value) {
+    return l;
+  } else {
+    return -1;}
+}
 }
